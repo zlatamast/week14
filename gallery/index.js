619f6b0d737a8a1433c750d708c37e0d0e7ef1e1
zlatamast <zlatamast@gmail.com>
2022-05-25 10:52:49 +0300

    add gallery and input_age

---
+++ gallery/index.js
@@ -0,0 +1,7 @@
+function swipeLeft() {
+    document.querySelector("#image").src="./image/99899304_original.jpg";
+}
+
+function swipeRight() {
+    document.querySelector("#image").src="./image/20220328-kot-post.jpg";
+}
